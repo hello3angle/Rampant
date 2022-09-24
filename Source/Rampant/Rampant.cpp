0b235507d29d38dff6b769814b571f364e61ae8f
Hello3angle <hello.3angle@gmail.com>
2022-09-24 21:54:42 +1000

    git init

---
+++ Source/Rampant/Rampant.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Rampant.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Rampant, "Rampant" );
+ 
---
+++ Source/Rampant/RampantGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RampantGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARampantGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARampantGameMode();
+};
+
+
+
